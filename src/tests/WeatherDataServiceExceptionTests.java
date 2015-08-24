3cae527656386babe460fe622665ad932d13f5c3
orenb2015 <orenbri@gmail.com>
2015-08-24 22:57:32 +0300

    added exception test.

---
+++ src/tests/WeatherDataServiceExceptionTests.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import finaljavaproject.WeatherDataServiceException;
+
+public class WeatherDataServiceExceptionTests {
+
+	@Test(expected = WeatherDataServiceException.class)
+	public void validException() throws WeatherDataServiceException {
+		throw new WeatherDataServiceException("something bad");
+	}
+
+	@Test
+	public void checkExceptionMessage() {
+		WeatherDataServiceException ex = new WeatherDataServiceException(
+				"something bad");
+		assertEquals("something bad", ex.getMessage());
+	}
+}
